7539e630107237fbde5b14d7dc0767d8af797bfa
Anyela <gilvargas1101@gmail.com>
2024-02-16 15:46:37 -0500

    Web pages

---
+++ myPageJS.js
@@ -0,0 +1,13 @@
+/*document.addEventListener('DOMContentLoaded', function (){
+	const proyect = document.getElementById('proyecto');
+	const atras = document.getElementById('atras');
+
+
+proyect.addEventListener('mouseover', function () {
+    atras.style.display = 'flex';
+  });
+
+ proyect.addEventListener('mouseout', function () {
+    atras.style.display = 'none';
+  });
+});*/
